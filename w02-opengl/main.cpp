61578162ee370bb99bc29c97fba26fdc5dcd4697
Andrew Danner <adanner@cs.swarthmore.edu>
2016-09-07 08:08:46 -0400

    indentation

--- w02-opengl/main.cpp
+++ w02-opengl/main.cpp
@@ -5,13 +5,13 @@
 int main(int argc, char *argv[])
 {
 
-	QApplication a(argc, argv);
-	QSurfaceFormat format;
-	format.setVersion(4,1);
-	format.setProfile(QSurfaceFormat::CoreProfile);
-	QSurfaceFormat::setDefaultFormat(format);
-	MainWindow w;
-	w.show();
+    QApplication a(argc, argv);
+    QSurfaceFormat format;
+    format.setVersion(4,1);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+    MainWindow w;
+    w.show();
 
-	return a.exec();
+    return a.exec();
 }
