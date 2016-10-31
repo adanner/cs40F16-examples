cebb8f57ed2c4baad68830de418cb4145072a901
Andrew Danner <adanner@cs.swarthmore.edu>
2016-10-31 09:26:03 -0400

    fshader

---
+++ w09-cuda-pt2/fshader.glsl
@@ -0,0 +1,18 @@
+#version 410
+
+uniform sampler2D Tex0;
+
+in vec4 color;
+in vec2 texCoord;
+
+out vec4 fragColor;
+
+
+void main() 
+{ 
+    fragColor = texture(Tex0,texCoord);
+    fragColor.a=1;
+    //fragColor = vec4(texCoord.x,texCoord.y,0,1.);
+    //fragColor = color;
+} 
+
